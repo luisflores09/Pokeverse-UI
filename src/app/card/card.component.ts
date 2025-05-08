d5dccb256877b663d862af9c0e279760baf8a6f9
Luis <iluisflores@yahoo.com>
2025-05-08 14:19:08 -0500

    rendered fetched pokemons into cards

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [CardModule, ButtonModule],
+  imports: [CardModule, CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  httpClient = inject(HttpClient);
+  allPokemon: any[] = [];
 
+  ngOnInit(): void {
+    this.fetchData();
+   }
+
+  fetchData() {
+    this.httpClient
+      .get('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .subscribe((data: any) => {
+        this.allPokemon = data.results;
+        console.log(this.allPokemon);
+      });
+  }
 }
