bd510d36d60b59b957b95cd8a19a6bf11452f8d0
Luis <iluisflores@yahoo.com>
2025-05-08 11:41:20 -0500

    created card component

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'app-card',
+  imports: [CardModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+
+}
