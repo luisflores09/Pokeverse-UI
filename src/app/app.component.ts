7290f4de9d7675d9d88214919d7f684defd1a250
Luis <iluisflores@yahoo.com>
2025-05-08 15:12:00 -0500

    added styling and displayed items using flex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardComponent } from './card/card.component';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [CardComponent],
+  imports: [CardComponent, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
